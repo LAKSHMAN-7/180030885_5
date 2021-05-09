21add20a48be41445ef56417ee8c664955bd7213
dell <dell@DESKTOP-0V4VLF9>
2021-05-09 23:38:24 +0530

    Fifteenth Commit

---
+++ PrimeorNot/src/PrimePack/PrimeornoT.java
@@ -0,0 +1,24 @@
+package PrimePack;
+import java.util.Scanner;
+public class PrimeornoT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        int c=0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                c++;
+            }
+        }
+        if(c==2){
+            System.out.println("Prime");
+        }
+        else{
+            System.out.println("Not a prime");
+        }
+
+	}
+
+}
