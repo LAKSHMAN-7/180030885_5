2b4daa7ae86e381eea1b9113b472f54c779aa924
dell <dell@DESKTOP-0V4VLF9>
2021-05-09 23:34:06 +0530

    Eleventh Commit

---
+++ Numwidtabspace/src/Numtabspace/NumberTabSpace.java
@@ -0,0 +1,13 @@
+package Numtabspace;
+import java.util.Scanner;
+public class NumberTabSpace {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+        for(int i=0;i<=10;i++){
+            System.out.print(i+"    ");
+        }
+	}
+
+}
