2de7cf795839c60f5c8f0d5d208c1f6db01b9dbf
dell <dell@DESKTOP-0V4VLF9>
2021-05-09 23:28:19 +0530

    Fourth Commit

---
+++ Cmdarguments/src/CmdorNot/Cmd.java
@@ -0,0 +1,21 @@
+package CmdorNot;
+import java.util.Scanner;
+public class Cmd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+
+	    
+        if(args.length==0){
+            System.out.println("No values");
+        }
+        else{
+            for (String v:args){
+                System.out.println(v);
+        }
+        }
+        sc.close();
+	}
+}
+
