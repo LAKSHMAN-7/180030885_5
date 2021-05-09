fe40cf7b893eaa1ce24ef99bf24b14df3d468034
dell <dell@DESKTOP-0V4VLF9>
2021-05-09 23:21:57 +0530

    First Commit

---
+++ ADC/src/Adc/AlDiCh.java
@@ -0,0 +1,20 @@
+package Adc;
+import java.util.Scanner;
+public class AlDiCh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+        char c = sc.nextLine().charAt(0);
+        if((c>=65 && c<=90) || (c>=97 && c<=122)){
+            System.out.println("alphabet");
+        }
+        else if((c>=33 && c<=47) || (c>=91 && c<=96) || (c>=123 && c<=126)){
+            System.out.print("Special Character");
+        }
+        else{
+            System.out.print("Digit");
+        }
+	}
+
+}
