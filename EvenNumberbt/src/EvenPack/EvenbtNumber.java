8016b564c23459f5828952307322e01143610233
dell <dell@DESKTOP-0V4VLF9>
2021-05-09 23:29:04 +0530

    Fifith Commit

---
+++ EvenNumberbt/src/EvenPack/EvenbtNumber.java
@@ -0,0 +1,15 @@
+package EvenPack;
+import java.util.Scanner;
+public class EvenbtNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+        for(int i=23;i<=57;i++){
+            if(i%2==0){
+            System.out.println(i);
+            }
+        }
+	}
+
+}
